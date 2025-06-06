e9883879a62a693c612cb1092d2c9fc99f8ad06c
probirkumar <probirsharma655@gmail.com>
2025-06-06 23:06:26 +0600

    Products add

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const menuTiger = document.querySelector('#menu-tiger');
+const mainMenu = document.querySelector('#main-menu');
+
+menuTiger.addEventListener('click', function () {
+    mainMenu.classList.toggle('show-menu');
+} )
+
